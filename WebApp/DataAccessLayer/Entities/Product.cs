5e5ac068882644b5eaa8d2846c8ce50dfe8da99a
sumedha-jhanji <sumedha_2001jhanji@yahoo.co.in>
2024-11-26 20:40:52 +0530

    added data access layer
    added data access layer

---
+++ WebApp/DataAccessLayer/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    [Table("tblProduct")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [StringLength(100)]
+        public string ProductName { get; set; }
+        [Column(TypeName ="varchar(200)")]
+        public string ProductCode { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
---
+++ WebApp/DataAccessLayer/TrainingDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class TrainingDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source=LAPTOP-VA38D82T;Initial Catalog=aspnetcoretraining;Integrated Security=true;Trust Server Certificate=true";
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
